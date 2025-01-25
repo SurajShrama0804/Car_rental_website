a5beeab9eafbc8b9a439867e735d3a6835447ae6
SurajShrama0804 <surajkumarind08@gmail.com>
2025-01-25 16:46:07 +0530

    changing and adding new files

---
+++ main.js
@@ -0,0 +1,1 @@
+Friends, I have not used JavaScript in this project, I use this in Future
--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-Friends, I have not used JavaScript in this project, I use this in Future
+document.querySelector('.login-btn').addEventListener('click', function(event) {
+    event.preventDefault();  // Prevent the form from submitting (if inside a form)
+    window.location.href = 'main.html';  // Redirect to index.html
+});
